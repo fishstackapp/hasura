dcddd410aecd996ef34856c8ed3232731981db14
Oleh Poliakov <106983325+OlehPoliakov@users.noreply.github.com>
2022-12-25 18:48:13 +0200

    update migration

--- migrations/default/1659390323836_init/up.sql
+++ migrations/default/1659390323836_init/up.sql
@@ -6,10 +6,8 @@ CREATE TABLE public.menu (
     image text NOT NULL,
     weight numeric NOT NULL,
     title text NOT NULL,
-    descriptions text NOT NULL,
+    ingredients text NOT NULL,
     price numeric NOT NULL
 );
 ALTER TABLE ONLY public.menu
     ADD CONSTRAINT menu_pkey PRIMARY KEY (id);
-CREATE TABLE public.admin ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "username" text NOT NULL, "password" text NOT NULL, PRIMARY KEY ("id") , UNIQUE ("username"));
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
